ec7351c2907d97acbd6629bb0ae59192553adfdf
LaMik <lam.michal.franc@gmail.com>
2011-04-06 16:54:34 +0200

    - Added first tests

---
+++ elearn/NHibernateTests/ModelTests/CourseModelTests.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using NHibernate;
+using NHibernate.Linq;
+using Models;
+using NHiberanteDal.DataAccess;
+
+namespace NHibernateTests.ModelTests
+{
+    [TestFixture]
+    public class CourseModelTest : TestBase
+    {
+
+        [SetUp]
+        public void AddSampleData()
+        {
+
+            //Test Group
+            GroupTypeModel testGroupType = new GroupTypeModel() { TypeName="Test Group Type" };
+            GroupModel testGroup = new GroupModel() { GroupName="Test Group", GroupType = testGroupType };
+
+            DataAccess.InTransaction(session => session.Save(testGroupType));
+            DataAccess.InTransaction(session => session.Save(testGroup));
+
+            //Test Froum
+            ForumModel testForum = new ForumModel() { Author="Test Author" , Name="Test Forum" };
+
+            DataAccess.InTransaction(session => session.Save(testForum));
+
+            //Test ShoutBox
+            ShoutboxModel testShoutBox = new ShoutboxModel() {};
+
+            DataAccess.InTransaction(session => session.Save(testShoutBox));
+
+            //Test Course
+            CourseTypeModel testCourseType = new CourseTypeModel() { TypeName = "Test Course Type" };
+            CourseModel testCourse = new CourseModel() { 
+                CourseType = testCourseType , CreationDate=DateTime.Now,
+                 Forum = testForum , Group = testGroup , Name="Test Course" ,
+                 ShoutBox = testShoutBox
+            };
+
+            DataAccess.InTransaction(session => session.Save(testCourseType));
+            DataAccess.InTransaction(session => session.Save(testCourse));
+        }
+
+        [Test]
+        public void CanAddCourse()
+        {
+            #region Arrange
+            CourseModel testCourse = new CourseModel()
+            {
+                CreationDate = DateTime.Now,
+                Name = "Test Course",
+                Group = new GroupModel() { GroupName = "Add Test Group",GroupType = new GroupTypeModel() { TypeName="Add Test Group Type" }},
+                ShoutBox = new ShoutboxModel(){},
+                Forum = new ForumModel(){ Name="Add Test Group Forum", Author="Test Author"},
+                CourseType = new CourseTypeModel(){TypeName = "Add Test Course Type"}
+            };
+
+            int beforeAddCount = 0;
+
+            using (var session = DataAccess.OpenSession())
+            {
+                beforeAddCount = session.Linq<CourseModel>().Count();
+            }
+            #endregion
+
+            #region Act
+
+            Repository<CourseModel>.Add(testCourse);
+
+            int afterAddCount = 0;
+
+            using (var session = DataAccess.OpenSession())
+            {
+                afterAddCount = session.Linq<CourseModel>().Count();
+            }
+
+            #endregion
+
+            #region Assert
+
+
+            Assert.That(afterAddCount, Is.EqualTo(beforeAddCount + 1));
+            
+            #endregion
+        }
+
+
+        [Test]
+        public void CanGetCourseByID()
+        {
+            #region Arrange
+            #endregion
+
+            #region Act
+            CourseModel course = Repository<CourseModel>.GetByID(1);
+
+	        #endregion
+
+            #region Assert
+            Assert.That(course, Is.Not.Null);
+            Assert.That(course.Name, Contains.Substring("Test Course"));
+            Assert.That(course.ID,Is.EqualTo(1));
+            #endregion
+        }
+
+
+        [Test]
+        public void GettingWrongIDReturnsNull()
+        {
+            #region Arrange
+            #endregion
+
+            #region Act
+            CourseModel course = Repository<CourseModel>.GetByID(-1);
+            #endregion
+
+            #region Assert
+            Assert.That(course, Is.Null);
+            #endregion
+        }
+
+
+
+        [Test]
+        public void CanGetCourseGroup()
+        {
+            #region Arrange
+            #endregion
+
+            #region Act
+
+            CourseModel course = Repository<CourseModel>.GetByID(1);
+            GroupModel group = course.Group;
+
+            #endregion
+
+            #region Assert
+            #endregion
+        }
+				
+
+        [Test]
+        public void CanGetCourseForum()
+        {
+            Assert.Fail();
+        }
+
+        [Test]
+        public void CanAddContent()
+        {
+            Assert.Fail();
+        }
+
+        [Test]
+        public void CanGetContent()
+        {
+            Assert.Fail();
+        }
+
+        [Test]
+        public void CanGetSurvey()
+        {
+            Assert.Fail();
+        }
+
+        [Test]
+        public void CanGetTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
---
+++ elearn/NHibernateTests/ModelTests/ProfileModelTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using NHibernate.Linq;
+using NHiberanteDal.DataAccess;
+using NHibernate;
+using Models;
+
+namespace NHibernateTests.ModelTests
+{
+    [TestFixture]
+    class ProfileModelTests : TestBase
+    {
+        [Test]
+        public  void CanAddProfile()
+        {
+            Repository<ProfileModel>.Add(new Models.ProfileModel());
+
+           int count = 0;
+           using (var session = DataAccess.OpenSession())
+           {
+               count = session.Linq<ProfileModel>().ToList().Count;
+           }
+
+           Assert.That(count,Is.EqualTo(1));
+
+        }
+    }
+}
