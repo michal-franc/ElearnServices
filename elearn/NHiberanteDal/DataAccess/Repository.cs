a11115eec64c24eab23ce32b2bec1a01e7a249cd
LaMik <lam.michal.franc@gmail.com>
2011-04-06 16:54:07 +0200

    - Added simple base repository

---
+++ elearn/NHiberanteDal/DataAccess/Repository.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NHibernate.Linq;
+
+namespace NHiberanteDal.DataAccess
+{
+    public static class Repository<T>
+        where T : class
+    {
+        public static void Add(T model)
+        {
+            DataAccess.InTransaction(session => session.Save(model));
+        }
+
+        public static int GetCount()
+        {
+            int count = 0;
+            using (var session = DataAccess.OpenSession())
+            {
+                count = session.Linq<T>().ToList().Count;
+            }
+
+            return count;
+        }
+
+        public static T GetByID(int p)
+        {
+            T returnedObject = null;
+            using (var session = DataAccess.OpenSession())
+            {
+                returnedObject = session.Get<T>(p);
+            }
+
+            return returnedObject;
+        }
+    }
+}
