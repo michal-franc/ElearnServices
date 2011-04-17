24137ff88f13175c8ee9b7027d3d5bcf376080ba
LaMik <lam.michal.franc@gmail.com>
2011-04-18 00:15:28 +0200

    -- Test : base class used for inMemory Tests

---
+++ elearn/NHibernateTests/InMemoryTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace NHibernateTests
+{
+    [TestFixture]
+    public abstract class InMemoryTest
+    {
+        protected SqlLiteTestDBAccess _database;
+
+        [SetUp]
+        public void CreateSqlLiteDB()
+        {
+            _database = new SqlLiteTestDBAccess();
+            Console.WriteLine("Test DataBase Created");
+        }
+
+
+        [TearDown]
+        public void DestroySqlLiteDB()
+        {
+            _database.Dispose();
+            Console.WriteLine("Test DataBaseDisposed");
+        }
+    }
+}
