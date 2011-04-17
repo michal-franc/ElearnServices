3a548e5923bc05551364902120b8c79794b711e5
LaMik <lam.michal.franc@gmail.com>
2011-04-18 00:11:05 +0200

    - Test : Various DB options (in memoryu and sql)

---
+++ elearn/NHibernateTests/DataAccesTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using NHiberanteDal.DataAccess;
+
+namespace NHibernateTests
+{
+    [TestFixture]
+    class DataAccesTests
+    {
+        [Test]
+        public void Can_create_access_to_db_in_memory()
+        {
+            SqlLiteTestDBAccess _database = new SqlLiteTestDBAccess();
+            using (var session = DataAccess.OpenSession())
+            {
+                Assert.That(session.Connection,Is.InstanceOfType(typeof(System.Data.SQLite.SQLiteConnection)));
+            }
+        }
+
+        [Test]
+        public void By_default_creates_access_to_db_in_sql()
+        {
+            using (var session = DataAccess.OpenSession())
+            {
+                Assert.That(session.Connection, Is.InstanceOfType(typeof(System.Data.SqlClient.SqlConnection)));
+            }
+        }
+
+    }
+}
