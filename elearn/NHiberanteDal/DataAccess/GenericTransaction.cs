a69a879f0c721b4269d9be5f13a3adc516900e27
LaMik <lam.michal.franc@gmail.com>
2011-04-18 21:01:59 +0200

    -- Added Generic Transaction

---
+++ elearn/NHiberanteDal/DataAccess/GenericTransaction.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NHibernate;
+
+namespace NHiberanteDal.DataAccess
+{
+    public interface IGenericTransaction : IDisposable
+    {
+        void Commit();
+        void Rollback();
+    }
+
+    public class GenericTransaction : IGenericTransaction
+    {
+        private readonly ITransaction _transaction;
+
+        public GenericTransaction(ITransaction transaction)
+        {
+            _transaction = transaction;
+        }
+
+        public void Commit()
+        {
+            _transaction.Commit();
+        }
+
+        public void Rollback()
+        {
+            _transaction.Rollback();
+        }
+
+        public void Dispose()
+        {
+            _transaction.Dispose();
+        }
+    }
+}
