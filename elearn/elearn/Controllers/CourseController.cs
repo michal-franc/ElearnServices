27fd21668bf860b0d5ebcfd317a99c514ab10e05
LaMik <lam.michal.franc@gmail.com>
2011-04-22 23:27:56 +0200

    - Added Course View and its simple Sub Pages

---
+++ elearn/elearn/Controllers/CourseController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using NHiberanteDal.DTO;
+
+namespace elearn.Controllers
+{
+    public class CourseController : Controller
+    {
+        //
+        // GET: /Course/
+
+        public ActionResult Index()
+        {
+            CourseService.CourseServiceClient client = new CourseService.CourseServiceClient();
+            List<CourseSignatureDto> courses = client.GetAllSignatures().ToList();
+            client.Close();
+
+            return View(courses);
+        }
+
+        // GET: /Course/Details/id
+        public ActionResult Details(int id)
+        {
+            CourseService.CourseServiceClient client = new CourseService.CourseServiceClient();
+            CourseDto course = client.GetById(id);
+            client.Close();
+
+            return View(course);
+        }
+
+        // GET: /Course/Tests/id
+        public ActionResult Tests(int id)
+        {
+            CourseService.CourseServiceClient client = new CourseService.CourseServiceClient();
+            var tests = client.GetAllTests(id);
+            return View(tests);
+        }
+
+        // GET: /Course/Surveys/id
+        public ActionResult Surveys(int id)
+        {
+            return View();
+        }
+
+    }
+}
