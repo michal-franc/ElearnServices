4e41375ad0bd36a829ebf7884dc4ba6f13a6b716
LaMik <lam.michal.franc@gmail.com>
2011-04-23 00:20:39 +0200

    - Added TestSignatureDto - Added TestTypeModelDto

---
+++ elearn/NHiberanteDal/DTO/TestSignatureDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NHiberanteDal.Models;
+using System.Runtime.Serialization;
+
+namespace NHiberanteDal.DTO
+{
+    [DataContract]
+    public class TestSignatureDto : DtoBaseClass<TestSignatureDto,TestModel>
+    {
+        [DataMember]
+        public  string Name { get; set; }
+        [DataMember]
+        public  DateTime CreationDate { get; set; }
+        [DataMember]
+        public  string Author { get; set; }
+        [DataMember]
+        public TestTypeModelDto TestType { get; set; }
+    }
+}
