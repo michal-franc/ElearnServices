e074c8afd8ed2bde81239fb4c66963f28fbe39e4
LaMik <lam.michal.franc@gmail.com>
2011-04-24 21:14:49 +0200

    -- Added Test Service

---
+++ elearn/ELearnServices/TestService.svc.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using NHiberanteDal.Models;
+using NHiberanteDal.DTO;
+using NHiberanteDal.DataAccess;
+
+namespace ELearnServices
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "TestService" in code, svc and config file together.
+    public class TestService : ITestService
+    {
+        public int AddTest(int courseId,TestDto test)
+        {
+            int id = -1;
+            DataAccess.InTransaction(session =>
+            {
+                var course = session.Get<CourseModel>(courseId);
+                TestModel model = TestDto.UnMap(test);
+                id = (int)session.Save(model);
+                course.Tests.Add(model);
+                session.Save(course);
+                
+            });
+
+            return id;
+
+        }
+
+        public void DeleteTest(TestDto test)
+        {
+            DataAccess.InTransaction(session =>
+            {
+                session.Delete(TestDto.UnMap(test));
+            });
+        }
+    }
+}
