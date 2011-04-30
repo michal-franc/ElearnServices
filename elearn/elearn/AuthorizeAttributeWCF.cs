a32116b18246ef508029b2ed770038f90896003d
LaMik <lam.michal.franc@gmail.com>
2011-05-01 00:49:01 +0200

    - Added : AuthorizeAttributeWcf which enables Roles check over WCF

---
+++ elearn/elearn/AuthorizeAttributeWCF.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Security.Principal;
+
+namespace elearn
+{
+    public class AuthorizeAttributeWCF : AuthorizeAttribute
+    {
+        protected override bool AuthorizeCore(HttpContextBase httpContext)
+        {
+            if (httpContext == null)
+            {
+                throw new ArgumentNullException("httpContext");
+            }
+            IPrincipal user = httpContext.User;
+            if (!user.Identity.IsAuthenticated)
+            {
+                return false;
+            }
+            if (this.Users.Length > 0 && !Enumerable.Contains<string>(this.Users.Split(','), user.Identity.Name, StringComparer.OrdinalIgnoreCase))
+            {
+                return false;
+            }
+            if (this.Roles.Length > 0)
+            {
+                string [] roles = this.Roles.Split(',');
+                var service = new ProfileService.ProfileServiceClient();
+                return service.IsUserInRoles(user.Identity.Name,roles);
+            }
+            return true;
+        }
+    }
+}
