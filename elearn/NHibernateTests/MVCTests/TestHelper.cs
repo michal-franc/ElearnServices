fa019cbba00cac9b290c7d53a01fd0284b32b118
LaMik <lam.michal.franc@gmail.com>
2011-05-07 18:18:23 +0200

    -- Added : TestHelper ConvertEntity to form collection and simple test

--- elearn/NHibernateTests/MVCTests/TestHelper.cs
+++ elearn/NHibernateTests/MVCTests/TestHelper.cs
@@ -9,6 +9,10 @@ using System.Web.Routing;
 using System.Security.Principal;
 using MvcContrib.TestHelper;
 using MvcContrib.TestHelper.Fakes;
+using System.Reflection;
+using NUnit.Framework;
+using NHiberanteDal.Models;
+using NHiberanteDal.DTO;
 
 namespace NHibernateTests.MVCTests
 {
@@ -36,5 +40,50 @@ namespace NHibernateTests.MVCTests
             return new ControllerContext(context.HttpContext, new RouteData(), context.Controller);
         }
 
+        public static FormCollection ConvertEntityToFormCollection(object entity)
+        {
+            FormCollection form = new FormCollection();
+            foreach (PropertyInfo prop in entity.GetType().GetProperties())
+            {
+                if (!prop.PropertyType.IsGenericType)
+                {
+                    var name = prop.Name;
+                    var value = prop.GetValue(entity, null) ?? String.Empty;
+
+                    form.Add(name, value.ToString());
+                }
+            }
+
+            return form;
+        }
+    }
+
+    [TestFixture]
+    public class TestHelperTests
+    {
+
+        [Test]
+        public void Can_convert_entity_to_form_collection()
+        {
+            #region Arrange
+            var profile = new ProfileModelDto() { Email = "test@test.com", ID = 1, IsActive = true, Name = "test", Role = "test" };
+            #endregion
+
+            #region Act
+
+            var form = TestHelper.ConvertEntityToFormCollection(profile);
+
+            #endregion
+
+            #region Assert
+            Assert.That(form.Count,Is.EqualTo(5));
+            Assert.That(form["Email"], Is.EqualTo("test@test.com"));
+            Assert.That(form["IsActive"], Is.EqualTo("True"));
+            Assert.That(form["Name"], Is.EqualTo("test"));
+            Assert.That(form["ID"], Is.EqualTo("1"));
+            Assert.That(form["Role"], Is.EqualTo("test"));
+            #endregion
+        }
+				
     }
 }
