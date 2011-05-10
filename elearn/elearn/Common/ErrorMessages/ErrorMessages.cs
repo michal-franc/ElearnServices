3ff406532f8fa341b86454109cd49417cc653d05
LaMik <lam.michal.franc@gmail.com>
2011-05-10 22:33:38 +0200

    -- Modify : Changed location of ErrorMessage -- added 2 new messages for course model

---
+++ elearn/elearn/Common/ErrorMessages/ErrorMessages.cs
@@ -0,0 +1,32 @@
+﻿namespace elearn.Common.ErrorMessages
+{
+    public static class Profile
+    {
+        public static string SetAsInactiveFailed
+        {
+            get { return "Set as inactive failed!"; }
+        }
+
+        public static string ProfileUpdateFail
+        {
+            get { return "Problem Updating Profile"; }
+        }
+
+        public static string RoleUpdateFail
+        {
+            get { return "Problem Updating Role"; }
+        }
+    }
+    public static class Course
+    {
+        public static string CourseAddToDbError
+        {
+            get { return "Problem in DB while creating Course"; }
+        }
+
+        public static string CourseModelUpdateError
+        {
+            get { return "Update course model error"; }
+        }
+    }
+}
