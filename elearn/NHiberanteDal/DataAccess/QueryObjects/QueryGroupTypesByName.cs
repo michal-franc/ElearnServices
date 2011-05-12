c621b7cf6665e5ec1c839d2d9ce8a78eb23327e4
LaMik <lam.michal.franc@gmail.com>
2011-05-12 23:02:23 +0200

    -- Added Query Group Type by Name

---
+++ elearn/NHiberanteDal/DataAccess/QueryObjects/QueryGroupTypesByName.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NHiberanteDal.DataAccess.QueryObjects
+{
+    public class QueryGroupTypesByName : IQueryObject
+    {
+        private string _value;
+        public QueryGroupTypesByName(string value)
+        {
+            _value = value;
+        }
+
+        public string Query
+        {
+            get { return String.Format
+                ("from GroupTypeModel c where c.TypeName = '{0}'", _value);
+            }
+        }
+    }
+}
