ec007cc9fcd05016d664cf7981642fa066156d90
LaMik <lam.michal.franc@gmail.com>
2011-05-14 22:49:25 +0200

    - modified shoutbox map to not load messages automaticliy

--- elearn/NHiberanteDal/Mappings/ShoutboxMaps.cs
+++ elearn/NHiberanteDal/Mappings/ShoutboxMaps.cs
@@ -14,7 +14,7 @@ namespace NHiberanteDal.Mappings
             Id(x => x.ID);
 
             //Many
-            HasMany(x => x.Messages).Cascade.All().Not.LazyLoad().KeyColumns.Add("ShoutBoxId");
+            HasMany(x => x.Messages).Cascade.All().KeyColumns.Add("ShoutBoxId");
 
         }
     }
@@ -25,7 +25,7 @@ namespace NHiberanteDal.Mappings
         public ShoutBoxMessageModelMap()
         {
             Id(x => x.ID);
-            Map(x => x.ShoutBoxId).Not.Nullable();
+            Map(x => x.ShoutBoxId);
             Map(x => x.Author).Not.Nullable();
             Map(x => x.Message).Not.Nullable();
             Map(x => x.TimePosted).Not.Nullable();
