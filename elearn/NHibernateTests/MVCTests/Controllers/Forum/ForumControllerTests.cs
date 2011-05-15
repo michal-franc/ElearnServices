b40fd71e4c759eb23e6b55677b6173131fc80931
LaMik <lam.michal.franc@gmail.com>
2011-05-15 22:30:22 +0200

    - forum mechanics tests

---
+++ elearn/NHibernateTests/MVCTests/Controllers/Forum/ForumControllerTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Rhino.Mocks;
+using elearn.Controllers;
+
+namespace NHibernateTests.MVCTests.Controllers.Forum
+{
+    public class BaseTest
+    {
+        protected MockRepository Mock;
+        //protected IForumService CourseService;
+        protected CourseController CourseController;
+    }
+
+
+    class ForumControllerTests
+    {
+    }
+}
---
+++ elearn/NHibernateTests/ServicesTests/ForumServiceTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace NHibernateTests.ServicesTests
+{
+    [TestFixture]
+    class ForumServiceTests : InMemoryWithSampleData
+    {
+        
+        [Test]
+        public void Can_get_topic_messages(int topicId)
+        {
+            #region Arrange
+            #endregion
+
+            #region Act
+
+            Assert.Fail();
+
+            #endregion
+
+            #region Assert
+            #endregion
+        }
+				
+    }
+}
