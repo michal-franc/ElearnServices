3d1ea75cdfbd5806213fcbb63aedb80b8e6d4eee
LaMik <lam.michal.franc@gmail.com>
2011-06-06 21:54:30 +0200

    - added new jquery , jquery ui and knockout js files

---
+++ elearn/elearn/Scripts/common.js
@@ -0,0 +1,27 @@
+﻿
+
+
+function ShowError(msg) {
+    $("<div id='errorModal'/>").dialog({
+        modal: true,
+        open: function () {
+            $("#errorModal").html(msg);
+        },
+        close: function () {
+            $("#errorModal").remove();
+        },
+        height: 400,
+        width: 500,
+        title: 'Error',
+        buttons: {
+            Ok: function () {
+                $(this).dialog("close");
+            }
+        },
+        dialogClass: 'alert'
+    });
+}
+
+function ShowError() {
+    ShowError("Application Error");
+}
