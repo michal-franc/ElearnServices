958698b06fc23d7258559352700d6217825243ec
LaMik <lam.michal.franc@gmail.com>
2011-07-12 20:45:26 +0200

    - added profile helper class

---
+++ elearn/elearn/Extensions/ProfileHelpers.cs
@@ -0,0 +1,27 @@
+﻿using System.Web;
+using System.Web.Mvc;
+using elearn.Session;
+
+namespace elearn.Extensions
+{
+    public static class ProfileHelpers
+    {
+        public static HtmlString GetUserName(this HtmlHelper helper)
+        {
+            var userData = SessionStateService.SessionState.GetCurrentUserDataFromSession();
+            return new HtmlString(userData.DisplayName);
+        }
+
+        public static HtmlString GetUserRole(this HtmlHelper helper)
+        {
+            var userData = SessionStateService.SessionState.GetCurrentUserDataFromSession();
+            return new HtmlString(userData.Role);
+        }
+
+        public static HtmlString GetUserId(this HtmlHelper helper)
+        {
+            var userData = SessionStateService.SessionState.GetCurrentUserDataFromSession();
+            return new HtmlString(userData.ProfileId.ToString());
+        }
+    }
+}
